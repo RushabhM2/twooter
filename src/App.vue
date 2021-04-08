fed781d28fc3e35dcaae1c113576ef3b1cf3dbda
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2021-04-08 12:03:31 +0300

    TwootItem component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,16 @@
 <template>
   <div id="app">
-    @{{user.username}} - {{ fullName  }}
-    <strong>Followers: </strong>{{followers}}
-    <button @click="followUser">Follow</button>
+    <UserProfile/>
   </div>
 </template>
 
 <script>
+import UserProfile from './components/UserProfile'
 
 export default {
   name: 'App',
-  data() {
-    return {
-      followers: 0,
-      user: {
-        id: 1,
-        username: 'RushabhMehta',
-        firstName: 'Rushabh',
-        lastName: 'Mehta',
-        email: 'rushabh.r.mehta@gmail.com',
-        isAdmin: true
-      }
-    }
-  },
-  watch: {
-    followers(newFollowerCount, oldFollowerCount) {
-      if (oldFollowerCount < newFollowerCount) {
-        console.log(`${this.user.username} has gained a follower`)
-      }
-    }
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
-    }
-  },
-  methods: {
-    followUser() {
-      this.followers++;
-    }
-  },
-  mounted() {
-    this.followUser();
+  components: {
+    UserProfile,
   }
 }
 </script>
@@ -51,9 +20,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  display: flex;
-  flex-direction: column;
+  min-height: 100vh;
+  background-color: #f3f5fa;
 }
 </style>
